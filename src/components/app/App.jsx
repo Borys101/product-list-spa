9c7cbefa112811c34085d42bd1081a24388b723c
Borys Ivliev <boris.ivlev.11@gmail.com>
2023-02-04 22:49:15 +0200

    App component which wrapping MainWrapper component and using in index.js has been added

---
+++ src/components/app/App.jsx
@@ -0,0 +1,11 @@
+import MainWrapper from "../mainWrapper/MainWrapper";
+
+const App = () => {
+    return (
+        <main className="app">
+                <MainWrapper />
+        </main>
+    )
+}
+
+export default App;
